e9d304cd972d2d5eeae37ebb3003b538ae40ac25
Shaheen Masood <shaheen@infiresoftware.com>
2018-05-25 11:44:42 -0700

    added newMessage and createMessage

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'jen@example.com',
+    text: 'Long time, no see!'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('New message: ', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,10 +14,21 @@ app.use(express.static( path.join(__dirname, '../public')));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+      from: 'mike@example.com',
+      text: 'Hey, how is everything?',
+      createdAt: 123
+  });
+
+  socket.on('createMessage', function(message) {
+    console.log('Create Message: ', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
+
 server.listen(port, () => {
   console.log(`server is running on port ${port}`);
 })
